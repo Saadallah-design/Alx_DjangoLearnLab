b820b544c105d4439913e8966e27a03c797514a8
Saadallah-design <saadalla.publishing@gmail.com>
2025-11-08 17:55:41 +0700

    adding the libriarian model to query

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -21,7 +21,7 @@ def get_books_in_library(library_name):
 def get_librarian_for_library(library_name):
     try:
         library = Library.objects.get(name=library_name)
-        librarian = library.librarian
+        librarian = Librarian.objects.get(library=library.pk)
         return librarian
     except Library.DoesNotExist:
         return None
