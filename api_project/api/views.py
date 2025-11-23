fd07641198466b98f234911e1261cfea6748c031
Saadallah-design <saadalla.publishing@gmail.com>
2025-11-23 16:17:47 +0700

    UPDATE: using generics in the views.py| setting up the first API Endpoint for task 1 week 12

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,12 +1,12 @@
 from api.models import Book
 from .serializers import BookSerializer
-from rest_framework.generics import ListAPIView
+from rest_framework import generics 
 
 # the following is a Generic Class-Based View
 # its logic implementation: Automatic. You only need to set attributes (queryset and serializer_class), 
 # and the parent class handles the rest.
 
-class BookList(ListAPIView):
+class BookList(generics.ListAPIView):
     # The queryset attribute tells the view what data to retrieve.
     queryset = Book.objects.all()
     # The serializer_class attribute tells the view how to format the data.
