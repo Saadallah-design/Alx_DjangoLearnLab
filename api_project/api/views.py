adcb7bfe9329486a44335474dab6b8fb45a6da01
Saadallah-design <saadalla.publishing@gmail.com>
2025-11-23 16:11:07 +0700

    setting up the first API Endpoint for task 1 week 12

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from api.models import Book
+from .serializers import BookSerializer
+from rest_framework.generics import ListAPIView
 
-# Create your views here.
+# the following is a Generic Class-Based View
+# its logic implementation: Automatic. You only need to set attributes (queryset and serializer_class), 
+# and the parent class handles the rest.
+
+class BookList(ListAPIView):
+    # The queryset attribute tells the view what data to retrieve.
+    queryset = Book.objects.all()
+    # The serializer_class attribute tells the view how to format the data.
+    serializer_class = BookSerializer
