0bb77ddd90de75d1d54425bdbff8617cd51dace8
Saadallah-design <saadalla.publishing@gmail.com>
2025-12-06 09:40:00 +0700

    Add: use of Django’s class-based views to handle CRUD operations | create, update, delete and bloglist and blog post detail views

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -1,10 +1,18 @@
 from django.urls import path
 from django.contrib.auth import views as auth_views
 from . import views
+from blog.views import PostListView, PostDetailView, PostCreateView, PostUpdateView, PostDeleteView
 
 urlpatterns = [
     path('login/', auth_views.LoginView.as_view(template_name='registration/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='registration/logged_out.html'), name='logout'),
     path('register/', views.register, name='register'),
     path('profile/', views.profile, name='profile'),
+
+    # post management URLs 
+    path('posts/', PostListView.as_view(), name='post_list'),
+    path('posts/<int:pk>/', PostDetailView.as_view(), name='post_detail'),
+    path('posts/new/', PostCreateView.as_view(), name='post_create'),
+    path('posts/<int:pk>/edit/', PostUpdateView.as_view(), name='post_update'),
+    path('posts/<int:pk>/delete/', PostDeleteView.as_view(), name='post_delete'),
 ]
--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render, redirect
 from .forms import CustomUserCreationForm
 from django.contrib.auth.decorators import login_required
+from django.views.generic import ListView, DetailView
+from .models import Post
+from django.contrib.auth.models import User
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.views.generic import CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
+from django.contrib import messages
+
 
 
 def register(request):
@@ -8,8 +16,10 @@ def register(request):
         form = CustomUserCreationForm(request.POST)
         if form.is_valid():
             form.save()
+            messages.success(request, 'Account created successfully. You can now log in.')
             return redirect('login')
     else:
+        messages.error(request, "Registration failed. Please check your details.")
         form = CustomUserCreationForm()
     return render(request, 'registration/register.html', {'form': form})
 
@@ -26,11 +36,76 @@ def profile(request):
         if user_form.is_valid() and profile_form.is_valid():
             user_form.save()
             profile_form.save()
+            messages.success(request, 'Your profile has been updated successfully.')
             return redirect('profile')
     else:
+        messages.error(request, "Failed to update your profile. Please check the details.")
         user_form = UserUpdateForm(instance=user)
         profile_form = ProfileForm(instance=user.profile)
     return render(request, 'registration/profile.html', {
         'user_form': user_form,
         'profile_form': profile_form
     })
+
+# defining views for post management (create, edit, delete)
+
+
+
+# to restrict user actions based on login status and ownership of posts
+# in class based views use LoginRequiredMixin. 
+class PostCreateView(LoginRequiredMixin, CreateView):
+    model = Post
+    fields = ['title', 'content']
+    template_name = 'blog/post_form.html'
+    # success_url = reverse_lazy('home') # no more needed since we override get_success_url
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        messages.success(self.request, 'Your post has been created successfully.')
+        return super().form_valid(form)
+    
+    def get_success_url(self):
+        return reverse_lazy('post_detail', kwargs={'pk': self.object.pk})
+    
+# UserPassesTestMixin to ensure only authors can edit or delete their posts
+# custom permission logic in test_func method. when used we can define test_func method
+# It lets you define a test_func method: only users for whom this method returns True can access the view.
+class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    model = Post
+    fields = ['title', 'content']
+    template_name = 'blog/post_form.html'
+    success_url = reverse_lazy('home')
+
+    # test_func to ensure only author can edit
+    def test_func(self):
+        post = self.get_object()
+        return self.request.user == post.author
+    
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        messages.success(self.request, 'Your post has been updated successfully.')
+        return super().form_valid(form)
+    
+class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    model = Post
+    template_name = 'blog/post_confirm_delete.html'
+    success_url = reverse_lazy('home')
+
+    # test_func to ensure only author can delete
+    def test_func(self):
+        post = self.get_object()
+        messages.success(self.request, 'Your post has been deleted successfully.')
+        return self.request.user == post.author
+    
+class PostListView(ListView):
+    model = Post
+    template_name = 'blog/home.html'
+    context_object_name = 'posts'
+    ordering = ['-published_date']
+    paginate_by = 5
+
+class PostDetailView(DetailView):
+    model = Post
+    template_name = 'blog/post_detail.html'
+    context_object_name = 'post'
+
